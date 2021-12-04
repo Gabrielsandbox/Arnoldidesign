2757035e4c783175952750783c1871cd38ad99d9
Gabriel Marinho <gabrielmarinhoworks@gmail.com>
2021-12-04 13:32:54 -0300

    ..

--- js/theme.js
+++ js/theme.js
@@ -9,7 +9,7 @@
 
 (function($) {
     "use strict";
-
+    
 
     $(document).on ('ready', function (){
 
@@ -21,20 +21,6 @@
           else sticky.removeClass('fixed');
 
         });
-        var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
 
         // ------------------------------ Theme Menu
         var menu= $("#mega-menu-holder");
